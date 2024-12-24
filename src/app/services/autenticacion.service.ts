6f20c2aede38fbfc8a6cddd7fa6b3ba8b3b039b9
Jairo <xair_1319@hotmail.com>
2024-12-24 11:23:40 -0500

    obtencion del token y redireccion a home

---
+++ src/app/services/autenticacion.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+import { environment } from '../../environments/environment.development';
+//tipados
+type LoginBody ={usuario:string,passwprd:string}
+type ResponseLogin = {message:string,token:string}
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AutenticacionService {
+
+  //inject
+ private httpClient = inject(HttpClient)
+// Variables
+private baseUrl : string = `${environment.API_URL}`;
+
+  login(body: LoginBody){
+    return lastValueFrom(this.httpClient.post<ResponseLogin>(`${this.baseUrl}/login`,body))
+
+  }
+
+
+}
