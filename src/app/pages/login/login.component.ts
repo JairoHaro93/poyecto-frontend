1fe6e1f39f3deb408b2c76bde26bb6ab391f62a8
Jairo Haro <xair_1319@hotmail.com>
2024-12-25 18:01:20 -0500

    eliminar console log

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormControl, FormGroup, ReactiveFormsModule } from  '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { AutenticacionService } from '../../services/autenticacion.service';
 import Swal from 'sweetalert2';
 
@@ -12,29 +12,22 @@ import Swal from 'sweetalert2';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-
-
-
   //variables
-  formLogin : FormGroup = new FormGroup({
+  formLogin: FormGroup = new FormGroup({
     usuario: new FormControl(),
-    password : new FormControl()
-  })
+    password: new FormControl(),
+  });
   //injectables
-  autenservice=inject(AutenticacionService)
-  router = inject(Router)
-  
-async onSubmit(){
+  autenservice = inject(AutenticacionService);
+  router = inject(Router);
 
-  try {
-    const response = await  this.autenservice.login(this.formLogin.value)
-    localStorage.setItem('token_proyecto',response.token)
-    this.router.navigateByUrl('/home')
-    console.log(response)
-  } catch ({error}:any) {
-    Swal.fire('Error',error.message,'error')
-    console.log(error)
+  async onSubmit() {
+    try {
+      const response = await this.autenservice.login(this.formLogin.value);
+      localStorage.setItem('token_proyecto', response.token);
+      this.router.navigateByUrl('/home');
+    } catch ({ error }: any) {
+      Swal.fire('Error', error.message, 'error');
+    }
   }
-}
-  
 }
