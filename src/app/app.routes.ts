ea3163bfc24fce237d68c1b6daade8c3296948df
Jairo Haro <xair_1319@hotmail.com>
2025-01-02 16:17:45 -0500

    funciones y componentes enlazados a sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,24 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
 import { tokenGuard } from './guards/token.guard';
-import { CrearusuarioComponent } from './pages/crearusuario/crearusuario.component';
+import { CrearusuarioComponent } from './pages/administrador/crearusuario/crearusuario.component';
+import { AdministradorComponent } from './pages/administrador/administrador.component';
+import { UsuariosComponent } from './pages/administrador/usuarios/usuarios.component';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent, canActivate: [tokenGuard] },
 
-  //sistema
-  { path: 'home/sistema/crearusuario', component: CrearusuarioComponent },
+  //administrador
+  {
+    path: 'home/administrador',
+    component: AdministradorComponent,
+    children: [
+      { path: 'usuarios', component: UsuariosComponent },
+      { path: 'crearusuario', component: CrearusuarioComponent },
+    ],
+  },
+
+  { path: '**', redirectTo: 'home' }, //ruta 404
 ];
