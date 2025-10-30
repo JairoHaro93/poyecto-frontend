c1ac15f95894d0e3b677dc37f2a825fab17e5120
Jairo Haro <xair_1319@hotmail.com>
2025-10-30 18:05:56 -0500

    Creacion componente para el manejo de olt

---
+++ src/app/pages/negocio/noc/olt/olt.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-olt',
+  standalone: true,
+  imports: [],
+  templateUrl: './olt.component.html',
+  styleUrl: './olt.component.css'
+})
+export class OltComponent {
+
+}
